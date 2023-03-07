fee39a05c691ebeed4fd00cfc9e821dbd0ebf658
Aayush Khatiwada <44517@nelnet.net>
2023-03-07 09:48:43 -0600

    Add project files.

---
+++ EntryTextAlignmentBug/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace EntryTextAlignmentBug;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ EntryTextAlignmentBug/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿namespace EntryTextAlignmentBug;
+
+public partial class MainPage : ContentPage
+{
+	private bool isVisible;
+	public bool IsVisible
+	{
+		get
+		{
+			return isVisible;
+		}
+		set
+		{
+			isVisible = value;
+			OnPropertyChanged(nameof(IsVisible));
+		}
+	}
+
+    private bool isVisible2;
+    public bool IsVisible2
+    {
+        get
+        {
+            return isVisible2;
+        }
+        set
+        {
+            isVisible2 = value;
+            OnPropertyChanged(nameof(IsVisible2));
+        }
+    }
+
+    public MainPage()
+	{
+		InitializeComponent();
+		IsVisible = true;
+		IsVisible2 = false;
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		IsVisible = !IsVisible;
+		IsVisible2 = !IsVisible2;
+	}
+}
+
